9fc1e888bf8a9d5ce918567aff6ab6f4a4b4a6b8
David Serrano <davidantonioserrano66@gmail.com>
2019-05-17 16:26:03 -0400

    Fixed the map rendering problem

--- main.lua
+++ main.lua
@@ -1,7 +1,5 @@
---THIS IS A COMMENT!!!
 local Gamestate = require 'hump.gamestate'
 
---mycode
 local playState = require 'states.play'
 
 function love.load()
@@ -9,11 +7,3 @@ function love.load()
 	Gamestate.registerEvents()
 end
 
-function love.update()
-
-end
-
-function love.draw()
-
-end
---My name is Matt
